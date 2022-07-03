d55f0947f973808838c54ce6dcd4fc7151f90070
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-07-03 21:51:52 +0400

    3rd_try

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,14 +1,12 @@
 package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
-    public static void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-        if (numberToGoUntil < toBreakWith) {
-            System.out.println("iterating till the end");
+    public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
             for (int i = 1; i <= numberToGoUntil; i++) {
-                System.out.println(i);
-            }
-        } else {
-            for (int i = 1; i <= numberToGoUntil; i++) {
+                if (numberToGoUntil < toBreakWith) {
+                    System.out.println("iterating till the end");
+                    break;
+                }
                 System.out.println(i);
                 if (i == toBreakWith) {
                     break;
@@ -16,10 +14,4 @@ public class NumberToBreakOn {
 
             }
         }
-
-    }
-
-    public static void main(String[] args) {
-        printNumbersUntilWithBreakOn(11, 10);
-    }
 }
